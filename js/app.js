03a55ccf4a8b2e7afd339c758225a5b2a627caa2
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-04-01 23:26:54 -0700

    basic fully functinaly tamagotchi finished

---
+++ js/app.js
@@ -0,0 +1,147 @@
+console.log("js loaded");
+
+class Tamagotchi {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 1;
+    this.sleepiness = 1;
+    this.boredom = 1;
+    this.age = 1;
+    this.status = "";
+    this.life = {
+      hunger: {
+        // Set the difficulty of the game
+        scale: getRandomScale(2, 5)
+      },
+      sleepiness: {
+        // Set the difficulty of the game
+        scale: getRandomScale(2, 5)
+      },
+      boredom: {
+        // Set the difficulty of the game
+        scale: getRandomScale(1, 5)
+      },
+      age: {
+        scale: 20
+      }
+    };
+  }
+}
+const getRandomScale = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+//
+
+const $time = $("#time");
+const $name = $(".name");
+const $age = $(".age");
+const $hunger = $(".hunger");
+const $sleepiness = $(".sleepiness");
+const $boredom = $(".boredom");
+const $food = $(".food");
+const $play = $(".play");
+const $nap = $(".lightToggleOff");
+// functions
+let interval;
+var newTamagotchi = new Tamagotchi();
+var setTime = 0;
+
+// console.log(newTamagotchi.life);
+const game = {
+  init() {
+    var getName = prompt("What would you like to name your tamagotchi?");
+
+    newTamagotchi = new Tamagotchi(getName);
+    clearInterval(interval);
+    setTime = 0;
+    // console.log(newTamagotchi);
+    game.setTimer(1000);
+    game.render();
+  },
+  setTimer() {
+    interval = window.setInterval(function() {
+      setTime += 1;
+      $time.text(`Timer: ${setTime} s`);
+      for (let him in newTamagotchi.life) {
+        if (setTime % newTamagotchi.life[him]["scale"] === 0) {
+          newTamagotchi[him] += 1;
+        }
+      }
+      game.render();
+    }, 1000);
+  },
+  render() {
+    $name.text(`'${newTamagotchi.name}'`);
+    $age.text(`Age: ${newTamagotchi.age}`);
+    $hunger.text(`Hunger: ${newTamagotchi.hunger}/10`);
+    $sleepiness.text(`Sleepiness: ${newTamagotchi.sleepiness}/10`);
+    $boredom.text(`Boredom: ${newTamagotchi.boredom}/10`);
+
+    //checks death
+    if (
+      newTamagotchi.hunger >= 10 ||
+      newTamagotchi.sleepiness >= 10 ||
+      newTamagotchi.boredom >= 10
+    ) {
+      game.death();
+    }
+  },
+  feed() {
+    if (newTamagotchi.hunger >= 1) {
+      newTamagotchi.hunger -= 1;
+    } else {
+      newTamagotchi.hunger = 0;
+    }
+    $(".welcome").attr(
+      "src",
+      "https://media.giphy.com/media/q5OV6r9dUvDMs/giphy.gif"
+    );
+  },
+  play() {
+    if (newTamagotchi.boredom >= 2) {
+      newTamagotchi.boredom -= 2;
+    } else {
+      newTamagotchi.boredom = 0;
+    }
+    $(".welcome").attr(
+      "src",
+      "https://media.giphy.com/media/sybXBcBpPHv2g/giphy.gif"
+    );
+  },
+  nap() {
+    if (newTamagotchi.sleepiness >= 3) {
+      newTamagotchi.sleepiness -= 3;
+    } else {
+      newTamagotchi.sleepiness = 0;
+    }
+    $("body").css("backgroundColor", "#1a0d0d");
+    $(".welcome").attr(
+      "src",
+      "https://media.giphy.com/media/OFD5J5AO2czeg/giphy.gif"
+    );
+    $("#time").css("color", "#ffffff");
+  },
+  death() {
+    clearInterval(interval);
+    newTamagotchi.status = "dead";
+    alert("Your Tamagotchi has died!");
+  }
+};
+
+// Buttons click
+$(".gameStart").on("click", game.init);
+// Feed me click
+$food.on("click", game.feed);
+// Play me click
+$play.on("click", game.play);
+// Nap time
+$nap.on("click", game.nap);
+// Wake time
+$(".lightToggleOn").on("click", () => {
+  $("body").css("backgroundColor", "#ffffff");
+  $(".welcome").attr(
+    "src",
+    "https://media.giphy.com/media/pdcEBvl3aokms/giphy.gif"
+  );
+});
